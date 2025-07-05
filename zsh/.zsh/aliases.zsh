1d69c06da8a5faaa7083905e29142354d08eadbb
Rodrigo Sasaki <rodrigopsasaki@gmail.com>
2025-07-05 20:26:48 -0300

    removing `ld` alias to be able to use the `ld` linker

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -7,6 +7,4 @@ alias ls='eza --group-directories-first --icons'          # Default view
 alias ll='eza -lah --group-directories-first --icons'     # Long + hidden + human-readable
 alias la='eza -a --group-directories-first --icons'       # Show all files, including dotfiles
 alias lt='eza --tree --level=2 --icons'                   # Tree view, 2 levels deep
-alias ld='eza -d */ --group-directories-first --icons'    # List directories only
-alias lx='eza -lah --sort=extension --icons'              # Group files by extension
 

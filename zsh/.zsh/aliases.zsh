2a896ec93a37b248a89101c059a324179be420b5
Fbisca <felippe.bisca@gmail.com>
2025-10-20 13:32:24 +0200

    Add Project Specific aliases

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -6,6 +6,13 @@ alias ls='eza --group-directories-first --icons'          # Default view
 alias ll='eza -lah --group-directories-first --icons'     # Long + hidden + human-readable
 alias la='eza -a --group-directories-first --icons'       # Show all files, including dotfiles
 
+
+# --- Project Specific Aliases ---
+alias h-svc="cd $HOME/developer/heritage/svc-platform"
+alias h-server="cd $HOME/developer/heritage/svc-platform/packages/server"
+alias h-webapp="cd $HOME/developer/heritage/web-app"
+alias h-admin="cd $HOME/developer/heritage/admin-panel"
+
 lt() {                                                    # Filtered tree view, 2 levels deep by default
   local level=${1:-2}
   shift

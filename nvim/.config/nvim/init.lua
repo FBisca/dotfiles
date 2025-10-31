fdb52ee070d819723c44c591c7cbbebd4bdda8a4
Fbisca <felippe.bisca@gmail.com>
2025-10-31 09:52:12 +0100

    Fix Lazy Module

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,19 @@
 vim.g.mapleader = " "
 
-vim.opt.rtp:prepend(vim.fn.stdpath("config") .. "/lazy/lazy.nvim")
+-- Bootstrap lazy.nvim if it's not installed
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
 require("lazy").setup({ import = "plugins" })
 
 require("plugins.nvimtree")
